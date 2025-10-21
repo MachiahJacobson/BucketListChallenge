92ded81bb13ebcfa3341d902ecdc2a9738bfac8e
Mac <macjacobson99@gmail.com>
2025-10-21 14:36:28 -0600

    Initial Commit

---
+++ BucketListChallenge/BucketListChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListChallengeApp.swift
+//  BucketListChallenge
+//
+//  Created by Jacobson, Machiah - Student on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListChallengeTests/BucketListChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListChallengeTests.swift
+//  BucketListChallengeTests
+//
+//  Created by Jacobson, Machiah - Student on 10/21/25.
+//
+
+import Testing
+@testable import BucketListChallenge
+
+struct BucketListChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
